beef2c4d4f21d4925f8c9918be43ddee48d505c7
Thiago Carvalho <thiagonbcarvalho@gmail.com>
2020-10-31 15:19:49 -0300

    Abstract the STDIN in io.Reader

--- history.go
+++ history.go
@@ -4,56 +4,38 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"os"
 	"os/exec"
 	"strings"
 )
-
-func WriteFile(filePath string, lines []string) error {
-	f, err := os.Create(filePath)
-	if err != nil {
-		return fmt.Errorf("cannot write to file %s, error %v", filePath, err)
-	}
-	for _, line := range lines {
-		f.WriteString(fmt.Sprintf("%s%s", line, "\n"))
-	}
-	defer f.Close()
-
-	return nil
-}
-
-func ReadInputFrom(r io.Reader) (string, error) {
-	reader := bufio.NewReader(r)
-	text, err := reader.ReadString('\n')
-	if err != nil {
-		return "", fmt.Errorf("cannot read input: %v", err)
-	}
-	return text, nil
-}
 
 func RunCommand(entrypoint string, args []string) (string, error) {
 	output, err := exec.Command(entrypoint, args...).Output()
 	if err != nil {
 		return "", err
 	}
+
 	return string(output), nil
 }
 
-func Run(w io.Writer) error {
+func Run(r io.Reader, w io.Writer) error {
+	// How can I print the $ only on CLI?
+	// Should I move the for loop to the CLI?
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Printf("$ ")
+		reader := bufio.NewReader(r)
 		text, err := reader.ReadString('\n')
-		text = text[:len(text)-1]
 		if err != nil {
-			return fmt.Errorf("error reading the input")
+			return fmt.Errorf("error reading the input: %e", err)
 		}
+		text = text[:len(text)-1]
 		if text == "exit" || text == "quit" {
 			break
 		}
-
-		ExecuteAndRecordCommand(text, w)
+		err = ExecuteAndRecordCommand(text, w)
+		if err != nil {
+			return err
+		}
 	}
+
 	return nil
 }
 
