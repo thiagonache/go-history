fa559c76eeee1f53ca806b8fc9cf75aca4954b21
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2020-12-30 14:45:34 -0300

    Revert "Add two functions to force golint to fail"
    This reverts commit e34ca66b35c8ca9514fc1042a4b13eaf17d58274.

--- history.go
+++ history.go
@@ -144,24 +144,5 @@ func (r Recorder) Shutdown() {
 	err := r.File.Close()
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		log.Println("Hi")
-	}
-}
-
-func f(x int) bool {
-	if x > 0 {
-		return true
-	} else { // MATCH /if.*return.*else.*outdent/
-		log.Printf("non-positive x: %d", x)
-	}
-	return false
-}
-
-func g(f func() bool) string {
-	if ok := f(); ok {
-		return "it's okay"
-	} else { // MATCH /if.*return.*else.*outdent.*short.*var.*declaration/
-		return "it's NOT okay!"
 	}
 }
